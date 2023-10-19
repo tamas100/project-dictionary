6eb2aab947625443b6e303543fe44e8347838513
Tamás Kis <t.kis502@gmail.com>
2023-10-19 14:54:58 +0200

    Create a form and validate the svalue of the search input

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # project-dictionary
-## A Karrierváltó Mentorprogram <https://programozaskarrier.hu/karriervalto/> 
-## Szótár - API - projektfeladat minta megoldás <https://programozas-karrier.teachable.com/courses/1413977/lectures/40889086> 
-## példafeladatának önálló megoldása a videó megtekintése elött.
+## A Karrierváltó Mentorprogram - <https://programozaskarrier.hu/karriervalto/> 
+## Szótár - API - projektfeladat minta megoldás - <https://programozas-karrier.teachable.com/courses/1413977/lectures/40889086> példafeladatának önálló megoldása a videó megtekintése elött.
+
+### Felhasznált API: <https://dictionaryapi.dev>
--- dictionary.js
+++ dictionary.js
@@ -1,3 +1,28 @@
+const $form = document.querySelector(".js-word-form");
+const $container = document.querySelector(".js-word-description");
+const $searchInput = document.querySelector("[name=word]");
+const $errorSection = document.querySelector(".js-error");
+
+function formSubmitted(event) {
+    event.preventDefault();
+    console.log('űrlap beküld')
+    const value = $searchInput.value.trim();
+    $container.innerHTML = '';
+    if (value.length > 0) {
+        $errorSection.innerHTML = '';
+
+    } else {
+        $errorSection.innerHTML =
+            'Sikertelen keresés!'
+    }
+}
+
+$form.addEventListener("submit", formSubmitted);
+
+
+
+
+
 function feldolgoz(beolvasottAdat) {
     console.log('adat', beolvasottAdat);
 }
