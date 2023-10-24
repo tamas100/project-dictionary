267ae626d20131ed4b8ef7a97f442bad7ceb2c72
Tamás Kis <t.kis502@gmail.com>
2023-10-24 09:37:58 +0200

    js code is ready

--- dictionary.js
+++ dictionary.js
@@ -5,50 +5,122 @@ const $searchInput = document.querySelector("[name=word]"); // Kiolvassuk az inp
 const $errorSection = document.querySelector(".js-error");
 const apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/"; // API URL minusz keresett szó
 
-function renderData(response) {
-    let html = '';
-    // validálás
-    if (Array.isArray(response) && response.length > 0) {
+function renderDefinitions(definitions) {
+    let html = ``;
+    for (let def of definitions) {
+        let example = '';
+        if (typeof def.example === 'string') {
+            example = `(e.g. ${def.example})`;
+        }
+        html += `        
+            <li>
+                ${def.definition} ${example}
+            </li>        
+        `
+    }
 
+    return html;
+}
+
+function renderSynonyms(synonymList) {
+    return (
+        synonymList
+            .map(text => `<a href="#" data-text="${text}">${text}</a>`) // kattinthatóak a szinonímák
+            .join(", ")
+    );
+}
+
+function renderMeaning(meaning) {
+    let html = `
+        <p>${meaning.partOfSpeech}</p>
+        <ol>${renderDefinitions(meaning.definitions)}</ol>        
+    `;
+
+    if (Array.isArray(meaning.synonyms) && meaning.synonyms.length > 0) {
+        // ha az első állítás igaz, akkor a másodikat is kiértékeljük
+        html += `
+            <p>Synonims: ${renderSynonyms(meaning.synonyms)}</p>
+        `
+    }
+
+    return html;
+}
+
+function renderWord(word) {
+    let phonetic = '';
+    if (typeof word.phonetic === 'string') {
+        phonetic = word.phonetic;
+    }
+    let html = `<h2>${word.word} ${phonetic}</h2>`; // wordDefinitions[0]-ban vannak
+    for (let meaning of word.meanings) {
+        html += renderMeaning(meaning); // másik függvényben megoldjuk
+    }
+
+    return html;
+}
+
+function renderResponse(wordList) {
+    let html = ``;
+    // validálás, sikertelen keresésnél nem kapunk array-t, hanem csak egy object-et.
+    if (Array.isArray(wordList) && wordList.length > 0) {
+        for (let word of wordList) {
+            html += renderWord(word);
+        }
     } else {
         $errorSection.innerHTML = `<p>A keresett szó nem található!</p>`;
     }
 
-    for (let item of response) {
-
-        html +=
-            `
-            <section class="meaning">
-                <h3>${item.word}</h3>
-                <p>${item.meanings[0].definitions[0].definition}</p>
-                <p>Synonym: ${item.meanings[0].synonyms[0]}</p>                
-            </section >
-        `
-    }
     $container.innerHTML = html;
+}
 
+// function renderResponse(wordDefinitions) {
+//     let html = ``;
+//     // validálás, sikertelen keresésnél nem kapunk array-t, hanem csak egy object-et.
+//     if (Array.isArray(wordDefinitions) && wordDefinitions.length > 0) {
+//         // let definitions = wordDefinitions[0];  
+//         html = renderWord(wordDefinitions[0]);
+//     } else {
+//         $errorSection.innerHTML = `<p>A keresett szó nem található!</p>`;
+//     }
+
+//     $container.innerHTML = html;
+// }
+
+function getApiUrl(word) {
+    return apiUrl + word;
+}
+
+function fetchWord(word) {
+    fetch(getApiUrl(word))
+        .then(response => response.json())
+        .then(renderResponse);
 }
 
 function formSubmitted(event) {
     event.preventDefault(); // Megakadályozzuk az oldal újratöltődését
 
-    const value = $searchInput.value.trim(); // Létrehozzuk a változót, ami a food értékét tartalmazza, 
+    const word = $searchInput.value.trim(); // Létrehozzuk a változót, ami a food értékét tartalmazza, 
     // a trim()-mel kiegészítve, hogy a szóközök ne jelentsenek problémát.
-    $searchInput.value = ''; // törli a keresőmezőt a keresés után
-    $container.innerHTML = '';       // / Ennek a tartalmát minden kereséskor törölni kell! $errorField.innerHTML = ''
+    $searchInput.value = '';          // törli a keresőmezőt a keresés után
+    $container.innerHTML = '';       // Ennek a tartalmát minden kereséskor törölni kell! $errorField.innerHTML = ''
 
-    if (value.length > 0) {         // Validáció
+    if (word.length > 0) {         // Validáció
         $errorSection.innerHTML = ''; // Ennek a tartalmát minden kereséskor törölni kell! $errorField.innerHTML = ''
 
-        fetch(apiUrl + value)
-            .then(data => data.json())
-            .then(renderData);
-
+        fetchWord(word)
     } else {
         $errorSection.innerHTML =
             'Sikertelen keresés!'
     }
-    console.log(value);
+    console.log(word);
+}
+
+function wordNavigation(event) {
+    event.preventDefault(); // Megakadályozzuk az oldal újratöltődését    
+    if (typeof event.target.dataset.text === 'string') {
+        fetchWord(event.target.dataset.text);
+    }
 }
 
 $form.addEventListener("submit", formSubmitted);
+$container.addEventListener("click", wordNavigation);
